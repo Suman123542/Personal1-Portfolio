57b96dd914ae3a5b038f2b72a9990de0cebe3eb2
Suman123542 <sumanswain0234@gmail.com>
2026-01-31 16:31:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    const menu = document.getElementById("menu");
+
+    function openMenu() {
+        menu.style.right = "0"
+    }
+
+    function closeMenu() {
+        menu.style.right = "-100%"
+    }
+</script>
